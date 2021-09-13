ac614b6ab67a8dfb27f0f28e7c1017de8c7453f5
RARARARARARARARARARARARA <rabramowitz6@gatech.edu>
2021-09-13 18:32:52 -0400

    added a framework for making a gui

--- src/bin/read_block.rs
+++ src/bin/read_block.rs
@@ -3,22 +3,11 @@ use std::{fs, io::Read};
 use kora::validation::NextBlock;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 fn main() {
     let mut f  = fs::File::open("blocks/b6").unwrap();
     let mut b = vec![]; 
     f.read_to_end(&mut b).unwrap();
     let b = bincode::deserialize::<NextBlock>(&b).unwrap();
     println!("{:?}",b);
+
 }
