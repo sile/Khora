6265d4735fe048cf0ced557d97b71508ac7a5e7a
Takeru Ohta <phjgt308@gmail.com>
2021-10-20 16:30:59 +0900

    Add port options

--- src/bin/entrypoint_example.rs
+++ src/bin/entrypoint_example.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate trackable;
 
-
 use fibers::sync::mpsc::{self, Receiver};
 use fibers::{Executor, Spawn, ThreadPoolExecutor};
 use futures::{Async, Future, Poll, Stream};
@@ -10,41 +9,45 @@ use plumcast::service::ServiceBuilder;
 use sloggers::terminal::{Destination, TerminalLoggerBuilder};
 use sloggers::Build;
 use std::net::SocketAddr;
+use structopt::StructOpt;
 use trackable::error::MainError;
 
+#[derive(Debug, Clone, StructOpt)]
+struct Opt {
+    #[structopt(long, default_value = "8334")]
+    port: u16,
 
-const DEFAULT_PORT: u64 = 8334;
+    #[structopt(long, default_value = "8334")]
+    peer_port: u16,
+}
 
 fn main() -> Result<(), MainError> {
-    let logger = track!(TerminalLoggerBuilder::new().destination(Destination::Stderr).level("debug".parse().unwrap()).build())?; // info or debug
-
-        
+    let opt = Opt::from_args();
+    let logger = track!(TerminalLoggerBuilder::new()
+        .destination(Destination::Stderr)
+        .level("debug".parse().unwrap())
+        .build())?; // info or debug
 
     /* server should use local ip or 0.0.0.0 client should connect through global ip address */
     println!("*{}", local_ipaddress::get().unwrap());
-    let addr: SocketAddr = format!("0.0.0.0:{}",DEFAULT_PORT).parse().unwrap();
-
-
-
-
+    let addr: SocketAddr = format!("0.0.0.0:{}", opt.port).parse().unwrap();
 
     let executor = track_any_err!(ThreadPoolExecutor::new())?;
     let service = ServiceBuilder::new(addr)
         .logger(logger.clone())
         .finish(executor.handle(), SerialLocalNodeIdGenerator::new()); // everyone is node 0 rn... that going to be a problem? I mean everyone has different ips...
-        
+
     let (message_tx, message_rx) = mpsc::channel();
     let node = TestNode {
-        node: NodeBuilder::new().logger(logger.clone()).finish(service.handle()),
+        node: NodeBuilder::new()
+            .logger(logger.clone())
+            .finish(service.handle()),
         receiver: message_rx,
+        opt,
     };
-    
-
-
 
     executor.spawn(service.map_err(|e| panic!("{}", e)));
     executor.spawn(node);
-
 
     std::thread::spawn(move || {
         use std::io::BufRead;
@@ -63,18 +66,16 @@ fn main() -> Result<(), MainError> {
         }
     });
 
-
     track_any_err!(executor.run()).unwrap();
 
     Ok(())
 }
 
-
-
 /// the node used to run all the networking
 struct TestNode {
     node: Node<Vec<u8>>,
     receiver: Receiver<Vec<u8>>,
+    opt: Opt,
 }
 impl Future for TestNode {
     type Item = ();
@@ -86,18 +87,34 @@ impl Future for TestNode {
             did_something = false;
 
             while let Async::Ready(Some(msg)) = track_try_unwrap!(self.node.poll()) {
-                println!("# MESSAGE: {:?}", String::from_utf8_lossy(&msg.message.payload[..]));
+                println!(
+                    "# MESSAGE: {:?}",
+                    String::from_utf8_lossy(&msg.message.payload[..])
+                );
 
-                println!("all plumtree peers: {:?}",self.node.plumtree_node().all_push_peers());
+                println!(
+                    "all plumtree peers: {:?}",
+                    self.node.plumtree_node().all_push_peers()
+                );
                 self.node.handle_gossip_now(msg, true);
                 did_something = true;
             }
             while let Async::Ready(Some(msg)) = self.receiver.poll().expect("Never fails") {
                 // this if statement is how the entrypoint runs. Type in *[ IPv4 ] here (in example is the following: "*192.168.0.101")
-                if msg.get(0) == Some(&42) /* * */ { // *192.168.0.101
-                    let addr: SocketAddr = track_any_err!(format!("{}:{}",String::from_utf8_lossy(&msg[1..]),DEFAULT_PORT).parse()).unwrap();
+                if msg.get(0) == Some(&42)
+                /* * */
+                {
+                    // *192.168.0.101
+                    let addr: SocketAddr = track_any_err!(format!(
+                        "{}:{}",
+                        String::from_utf8_lossy(&msg[1..]),
+                        self.opt.peer_port
+                    )
+                    .parse())
+                    .unwrap();
                     let nodeid = NodeId::new(addr, LocalNodeId::new(0));
-                    self.node.dm("hello!".as_bytes().to_vec(),&vec![nodeid],true);
+                    self.node
+                        .dm("hello!".as_bytes().to_vec(), &vec![nodeid], true);
                 } else {
                     self.node.broadcast(msg);
                 }
